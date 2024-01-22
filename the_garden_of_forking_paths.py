8793ba393629a0a518acb722b76f6a4ea7a1f80f
carrowkeel <6824521+carrowkeel@users.noreply.github.com>
2024-01-22 15:29:07 +0200

    adding models

---
+++ the_garden_of_forking_paths.py
@@ -0,0 +1,2 @@
+# Is your LLM rational or are you just projecting?
+# Are you rational or is your AI just projecting?
--- the_great_misadventure.py
+++ the_great_misadventure.py
@@ -1,1 +1,2 @@
-## Hello
+# Der Speigel im Spiegel
+# Can you make your way through the tormented inner world of an LLM?
