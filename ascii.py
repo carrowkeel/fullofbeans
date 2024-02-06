c001f41420db4492fee934383b693e26012c14ca
carrowkeel <6824521+carrowkeel@users.noreply.github.com>
2024-02-06 20:42:44 +0200

    fixes

--- ascii.py
+++ ascii.py
@@ -1,6 +1,7 @@
+# Some names are shorter than others
 
 async def run():
-	user_input = await js.interact('name')
+	user_input = await js.interact('form', 'Write a name')
 	prompt = f'Write this name in ASCII art: {user_input}'
 	result = await js.query(prompt)
 	js.draw('ascii', 'text', result)
